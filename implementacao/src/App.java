2dced200d7cf0d6ad539a9d665ce9b1eb23d8774
guizombas <guilherme.gabriel.532@gmail.com>
2021-08-26 22:55:10 -0300

    cru usuarios e menu de professor

--- implementacao/src/App.java
+++ implementacao/src/App.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.lang.StackWalker.Option;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import entidades.*;
 import entidades.enums.*;
@@ -11,6 +14,7 @@ public class App {
 
     private static List<Usuario> listaUsuarios = new LinkedList<>();
     private static List<Turma> listaTurmas = new LinkedList<>();
+    private static Usuario logado;
 
     // #region Utilidades
     /**
@@ -44,7 +48,8 @@ public class App {
                 }
             } catch (ClassCastException e) {
                 System.err.println(
-                        "Erro: Fallha ao fazer o casting dos objetos salvos no arquivo para Usuario, não foi possível carregar os usuários.");
+                    "Erro: Fallha ao fazer o casting dos objetos salvos no arquivo para Usuario, não foi possível carregar os usuários."
+                );
             }
             leitura.fecharArquivo();
         }
@@ -130,45 +135,192 @@ public class App {
         teclado.nextLine();
         return opcao;
     }
-    private static void criarUsuario(Scanner teclado) {
+    private static int seletorDeTipoDeUsuario(Scanner teclado){
         limparTela();
 
-        Usuario novo = null;
         System.out.println("Qual o tipo do usuário?");
         System.out.println("1 - Aluno");
         System.out.println("2 - Professor");
-        System.out.println("2 - Secretaria");
+        System.out.println("3 - Secretaria");
         System.out.println("0 - Cancelar");
 
         int tipo = teclado.nextInt();
+        teclado.nextLine();
+        return tipo;
+    }
+    private static void criarUsuario(Scanner teclado) {
 
-        switch (tipo) {
-            case 1:
-                /// Criar tipo aluno aqui.. pergunta nome etc..
-                break;
-        
-            default:
-                break;
+        Usuario novo = null;
+
+        int tipo = seletorDeTipoDeUsuario(teclado);
+
+        if ( tipo>=1 && tipo<=3 ){
+            System.out.print("Insira o e-mail: ");
+            String email = teclado.nextLine();
+            System.out.print("Insira a senha: ");
+            String senha = teclado.nextLine();
+
+            switch (tipo) { 
+
+                case 1:
+                case 2:
+                    System.out.print("Insira o nome: ");
+                    String nome = teclado.nextLine();
+                    if (tipo==1){
+                        System.out.print("Insira a CPF: ");
+                        String cpf = teclado.nextLine();
+                        novo = new Aluno( email, senha, nome, cpf );
+                    }
+                    else{
+                        novo = new Professor( email, senha, nome );
+                    }
+                    
+                    break;
+                case 3:
+                    System.out.print("Insira o telefone: ");
+                    long telefone = teclado.nextLong();
+                    teclado.nextLine();
+                    novo = new Secretaria(email, senha, telefone);
+                    break;
+            
+                default:
+                    break;
+            }
+
+            if (novo!=null){
+                listaUsuarios.add(novo);
+                salvarUsuariosNoArquivo(arquivoUsuarios);
+                System.out.println("Usuário criado: ");
+                System.out.println(novo);
+            }
+
+            pausa(teclado);
         }
 
-        listaUsuarios.add(novo);
-        salvarUsuariosNoArquivo(arquivoUsuarios);
     }
-    private static void verUsuarios() {
+    // ve usuários sem filtro (todos usuários)
+    private static void verUsuarios(){
+        verUsuarios( null );
+    }
+    private static void verUsuarios( Predicate<Usuario> filtro ) {
         limparTela();
-
-        // Fazer função para ver todos os usuários
-
-        // listaUsuarios.add(novo); Lembrar de adicionar na lista pra salvar no arquivo
-        salvarUsuariosNoArquivo(arquivoUsuarios);
+        Stream<Usuario> usuariosStream =  listaUsuarios.stream() ;
+        // se existir filtro stream é filtrada de acordo
+        if ( filtro != null )
+            usuariosStream = usuariosStream.filter(filtro);
+        
+        // printa todos usuários da stream
+        usuariosStream.forEach(System.out::println);
+    }
+    private static void verTurmas(){
+        verTurmas( null );
+    }
+    private static void verTurmas( Predicate<Turma> filtro ) {
+        limparTela();
+        Stream<Turma> turmasStream =  listaTurmas.stream() ;
+        // se existir filtro, stream é filtrada de acordo
+        if ( filtro != null )
+            turmasStream = turmasStream.filter(filtro);
+        
+        // printa todas turmas da stream
+        turmasStream.forEach(System.out::println );
     }
     private static void atualizarUsuario(Scanner teclado) {
-        limparTela();
+        verUsuarios( u -> !u.getClass().equals(Secretaria.class) );
+        System.out.println("Digite o ID do aluno que deseja atualizar os dados");
+        int usuarioId = teclado.nextInt();
+        teclado.nextLine();
+
+        //encontra turma com id digitado
+        Optional<Usuario> usuarioEscolhido = Optional.empty();
+        for ( Usuario u: listaUsuarios ){
+            if (u.getId()==usuarioId && !u.getClass().equals(Secretaria.class)){
+                usuarioEscolhido = Optional.of(u);
+                break;
+            }
+        }
+
+        usuarioEscolhido.ifPresentOrElse( u -> {
+            System.out.println("(Para todos os campos, digitar até 3 caracteres ignora a edição)");
+            System.out.println("Email atual: " + u.getEmail());
+            System.out.print("Novo email: ");
+            String email = teclado.nextLine();
+            System.out.print("Nova senha: ");
+            String senha = teclado.nextLine();
+
+            if (u.getClass().equals(Aluno.class)){
+                Aluno a = (Aluno) u;
+                System.out.println("Nome atual: " + a.getNome());
+                System.out.print("Novo nome: ");
+                String nome = teclado.nextLine();
+                System.out.println("CPF atual: " + a.getcpf());
+                System.out.print("Novo CPF: ");
+                String cpf = teclado.nextLine();
+                a.atualizar(
+                    email.length() > 3 ? Optional.of(email) : Optional.empty(), 
+                    senha.length() > 3 ? Optional.of(senha) : Optional.empty(), 
+                    nome.length() > 3 ? Optional.of(nome) : Optional.empty(), 
+                    cpf.length() > 3 ? Optional.of(cpf) : Optional.empty()
+                );
+            }
+            else if (u.getClass().equals(Professor.class)){
+                Professor p = (Professor) u;
+                System.out.println("Nome atual: " + p.getNome());
+                System.out.print("Novo nome: ");
+                String nome = teclado.nextLine();
+                p.atualizar(
+                    !email.equals("0") ? Optional.of(email) : Optional.empty(), 
+                    !senha.equals("0") ? Optional.of(senha) : Optional.empty(), 
+                    !nome.equals("0") ? Optional.of(nome) : Optional.empty()
+                );
+            }
+
+            System.out.println(u);
+            salvarUsuariosNoArquivo(arquivoUsuarios);
+
+        }, () -> System.out.println("Usuário não encontrado"));
+
+        pausa(teclado);
 
         // Fazer função para atualizar dado do usuário
 
-        // listaUsuarios.add(novo); Lembrar de adicionar na lista pra salvar no arquivo
-        salvarUsuariosNoArquivo(arquivoUsuarios);
+    }
+
+    private static int menuProfessor(Scanner teclado){
+        limparTela();
+        Professor professorLogado = (Professor) logado ;
+        System.out.println("Olá, professor " + professorLogado.getNome());
+        System.out.println("O que deseja fazer?");
+        System.out.println("==========================");
+        System.out.println("1 - Ver alunos por disciplina lecionada");
+        System.out.println("0 - Sair");
+
+        int opcao = teclado.nextInt();
+        teclado.nextLine();
+        return opcao;
+
+    }
+    private static void verAlunosPorDisciplina(Scanner teclado){
+        limparTela();
+
+        verTurmas( t -> t.getProfessor().equals(logado) );
+
+        System.out.println("Digite o ID da turma que deseja consultar os alunos matriculados nela");
+        int turmaId = teclado.nextInt();
+        teclado.nextLine();
+
+        //encontra turma com id digitado
+        Optional<Turma> turmaEscolhida = Optional.empty();
+        for ( Turma t: listaTurmas ){
+            if (t.getId()==turmaId && t.getProfessor().equals(logado)){
+                turmaEscolhida = Optional.of(t);
+                break;
+            }
+        }
+
+        turmaEscolhida.ifPresent( t -> t.buscarAlunos().forEach( System.out::println ) );
+
+        pausa(teclado);
     }
 
     // #endregion
@@ -187,42 +339,50 @@ public class App {
         salvarUsuariosNoArquivo(arquivoUsuarios);
         listaUsuarios = carregarUsuariosDoArquivo(arquivoUsuarios);
 
-        Usuario logado = login(teclado, listaUsuarios);
+        logado = login(teclado, listaUsuarios);
         if (logado == null)
             System.err.println("Email ou senha incorretos!");
-        else
+        else{
             System.out.println("Login efetuado com sucesso!");
-        pausa(teclado);
-
-        if (logado.getClass().equals(Secretaria.class)) {
-            int opcao = 0;
-            do {
-                opcao = menuSecretaria(teclado);
-                if (opcao==1)
-                    gerarCurriculoSemestral(teclado);
-                else if (opcao==2) {
-                    int acao = menuCRUDUsuario(teclado);
-                    switch (acao) {
-                        case 1:
-                            criarUsuario(teclado);
-                            break;
-                        case 2:
-                            verUsuarios();
-                            break;
-                        case 3:
-                            atualizarUsuario(teclado);
-                            break;
-                        default:
-                            break;
+            pausa(teclado);
+    
+            if (logado.getClass().equals(Secretaria.class)) {
+                int opcao = 0;
+                do {
+                    opcao = menuSecretaria(teclado);
+                    if (opcao==1)
+                        gerarCurriculoSemestral(teclado);
+                    else if (opcao==2) {
+                        int acao = menuCRUDUsuario(teclado);
+                        switch (acao) {
+                            case 1:
+                                criarUsuario(teclado);
+                                break;
+                            case 2:
+                                verUsuarios();
+                                break;
+                            case 3:
+                                atualizarUsuario(teclado);
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                }
-
-            } while (opcao!=0);
-            
-        } else if (logado.getClass().equals(Professor.class)) {
-            // Pesquisar por diciplina para ver alunos
-        } else if (logado.getClass().equals(Aluno.class)) {
-            // Matricular em uma disciplina (turma) validando matéria obrigatória e optativa
+    
+                } while (opcao!=0);
+                
+            } else if (logado.getClass().equals(Professor.class)) {
+                int opcao = 0;
+                do {
+                    opcao = menuProfessor(teclado);
+                    if (opcao==1)
+                        verAlunosPorDisciplina(teclado);
+    
+                } while (opcao!=0);
+            } else if (logado.getClass().equals(Aluno.class)) {
+                // Matricular em uma disciplina (turma) validando matéria obrigatória e optativa
+            }
         }
+        
     }
 }
